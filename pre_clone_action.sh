a2d5dd89bb02411d55122740273ccb3d5df65ace
446432510 <80037489+446432510@users.noreply.github.com>
2025-12-26 19:56:37 +0800

    Update pre_clone_action.sh
    Signed-off-by: 446432510 <80037489+446432510@users.noreply.github.com>

--- pre_clone_action.sh
+++ pre_clone_action.sh
@@ -51,7 +51,7 @@ git clone --depth 1 -b $REPO_BRANCH $REPO_URL $BUILD_DIR
 # 加入QOSMATE
 cd "$GITHUB_WORKSPACE/action_build/package"
 git clone --depth 1 https://github.com/hudra0/qosmate.git qosmate
-mv qosmate/luci-app-qosmate .
+mv qosmate/luci-app-qosmate 
 rm -rf qosmate
 
 
